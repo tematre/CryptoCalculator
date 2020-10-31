b1b55f21c98b86fe2fe11160aa225c63d0e6d28f
Artem Tregubov <artem.tregubov@mail.ru>
2020-10-31 15:38:53 +0700

    Add sone changes

--- Backend/index.js
+++ Backend/index.js
@@ -130,7 +130,7 @@ console.log(result);
 
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello world!')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
